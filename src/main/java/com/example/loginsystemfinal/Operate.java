ce88ac804711f65c95bd47698e5e03528a7905c0
fzkn4 <tomogsokin@gmail.com>
2023-12-30 10:20:19 +0800

    minor changes on Operate window's apperance

--- src/main/java/com/example/loginsystemfinal/Operate.java
+++ src/main/java/com/example/loginsystemfinal/Operate.java
@@ -1,6 +1,7 @@
 package com.example.loginsystemfinal;
 
 import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -30,6 +31,18 @@ public class Operate implements Initializable{
     @FXML
     private Text labName;
     public static String display;
+    @FXML
+    private MFXTextField name;
+    @FXML
+    private MFXTextField courseYear;
+    @FXML
+    private MFXTextField rfid;
+
+    @FXML
+    private MFXTextField timeIn;
+
+    @FXML
+    private MFXTextField timeOut;
 
 
     @FXML
@@ -42,11 +55,7 @@ public class Operate implements Initializable{
         operateStage.hide();
     }
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        Functions.clock(date, time);
-        labName.setText(display.toUpperCase());
-    }
+
     @FXML
     void keyPressed(KeyEvent event) throws IOException {
         if (event.getCode() == KeyCode.ESCAPE) {
@@ -55,5 +64,15 @@ public class Operate implements Initializable{
             Stage operateStage = (Stage) operatePane.getScene().getWindow();
             operateStage.hide();
         }
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Functions.clock(date, time);
+        labName.setText(display.toUpperCase());
+        name.setText("jerald gwapo");
+        courseYear.setText("BSCS - 2");
+        rfid.setText("a80912");
+        timeIn.setText("10:12 am");
+        timeOut.setText("11:23 am");
     }
 }
